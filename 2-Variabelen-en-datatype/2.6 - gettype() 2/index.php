717fd8e7d9c321c15aefa3c658b7006203d0de0e
Aydin Maleki <2148169@talnet.nl>
2021-12-02 13:51:13 +0100

    2.6

---
+++ 2-Variabelen-en-datatype/2.6 - gettype() 2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$a = "Aydin";
+echo gettype($a) . "<br>";
+
+$b = false;
+echo gettype($b) . "<br>";
+
+$c = 13.13;
+echo gettype($c) . "<br>";
+
+$d = NULL;
+echo gettype($d) . "<br>";
+
+$e = "Hi";
+echo gettype($e) . "<br>";
+?>
+
+</body>
+</html> 
