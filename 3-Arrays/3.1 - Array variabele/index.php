9db49e030bfc880805ef7655baedbb2eb7812e41
Aydin Maleki <2148169@talnet.nl>
2021-12-03 13:37:28 +0100

    3.1

---
+++ 3-Arrays/3.1 - Array variabele/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$Movies = array ("Shutter island", "Lucy", "Limitless");
+echo "<li>$Movies[0]</li><li>$Movies[1]</li><li>$Movies[2]</li>";
+?>
+
+</body>
+</html> 
