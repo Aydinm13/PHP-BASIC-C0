ea583fda2aa8897d4eb0ccb674630c513ffed786
Aydin Maleki <2148169@talnet.nl>
2021-12-03 14:04:22 +0100

    3.2

---
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$mydog = array ("Chahid", "MO", "Jaren");
+echo $mydog[0] .' '.  $mydog[1] .' '.  $mydog[2];
+echo "<br><hr>";
+$mydog[2] = 'Poep';
+echo $mydog[0] .' '.  $mydog[1] .' '.  $mydog[2];
+?>
+</body>
+</html> 
