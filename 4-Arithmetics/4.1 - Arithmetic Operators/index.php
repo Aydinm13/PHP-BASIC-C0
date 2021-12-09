9650ba90a40fe0f2b6dc709bfa2e813ec9818366
Aydin Maleki <2148169@talnet.nl>
2021-12-09 13:28:13 +0100

    4.1

---
+++ 4-Arithmetics/4.1 - Arithmetic Operators/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$getal1 = 3;
+$getal2 = 9;
+echo $getal1 + $getal2;
+
+echo "<br><hr>";
+
+
+$getal1 = 3;
+$getal2 = 9;
+echo $getal1 - $getal2;
+
+echo "<br><hr>";
+
+$getal1 = 3;
+$getal2 = 9;
+echo $getal1 * $getal2;
+echo "<br><hr>";
+
+$getal1 = 3;
+$getal2 = 9;
+echo $getal1 / $getal2;
+
+?>
+</body>
+</html> 
