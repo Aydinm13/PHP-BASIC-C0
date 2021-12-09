f357d75bdf87296a68ce742c9c207b25b989dfe0
Aydin Maleki <2148169@talnet.nl>
2021-12-09 13:58:53 +0100

    4.2

---
+++ 4-Arithmetics/4.2 -Assignment Operators/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$getal1 = 13;
+$getal1 += 4;
+echo $getal1;
+echo "<br>";
+
+
+$getal2 = 5;
+$getal2 -= 10;
+echo $getal2;
+echo "<br>";
+
+$getal3 = 4;
+$getal3 *= 7;
+echo $getal3;
+echo "<br>";
+
+$getal4 = 4;
+$getal4 /= 2;
+echo $getal4;
+
+?>
+</body>
+</html> 
