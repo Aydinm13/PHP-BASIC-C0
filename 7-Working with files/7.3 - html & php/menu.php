c9b544f16268998a9b2a9cb1f50e890bbee15b3e
Aydin Maleki <2148169@talnet.nl>
2021-12-17 12:56:59 +0100

    7.3

---
+++ 7-Working with files/7.3 - html & php/menu.php
@@ -0,0 +1,16 @@
+<?php require 'database.php'; ?>
+<html>
+<head>
+</head>
+<body>
+
+<div class = "menu">
+  <ul> 
+     <li> Home </li>
+     <li> overmij</li>
+     <li> Contact</li>
+    </ul>
+</div>
+
+</body>
+</html>
